87f573dab1c1379c9caaed765b78c3d0b83949d5
Dmytro Petrov <sidodji228007@gmail.com>
2025-06-05 21:06:34 +0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -9,10 +9,8 @@ function calculateRentalCost(days) {
   const longTermDiscount = 50;
 
   let rentalSum = 0;
-
-  for (let i = 0; i < days; i++) {
-    rentalSum += costPerDay;
-  }
+  
+  rentalSum = costPerDay * days;
 
   if (days >= 7) {
     return rentalSum - longTermDiscount;
